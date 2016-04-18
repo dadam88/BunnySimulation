6c70fa7cf26cd9b88fcd97922cfe2948a1e7f210
dadam88 <derekadam1988@gmail.com>
2016-04-18 17:39:06 -0500

    initial commit

---
+++ board.py
@@ -0,0 +1,56 @@
+import bunnysim
+import random
+
+def printgrid(board):
+	for i in range(10):
+		for j in range(10):
+			if not board[i][j] == 0 and not board[i][j].radioactive:
+				print board[i][j].sex,
+			else:
+				print 0,
+		print ""
+	print "-" * 72
+
+
+#Maybe use filter with function that compares  == 0
+def getemptycoords(board):
+	emptycoord = list()
+	for i in range(10):
+		for j in range(10):
+			if board[i][j] == 0:
+				emptycoord.append((i,j))
+	return emptycoord
+
+def main():
+	#Initialize a 3 by 3 board filled with 0's
+	# board = list() #2d array [col][row], very confusing in python compared to c++, jeez
+	# for row in range(10):
+	# 	board.append([])
+	# 	for col in range(10):
+	# 		board[row].append(0)
+	board = [[0 for col in range(10)] for row in range(10)]
+
+	printgrid(board)
+
+	def spawn((a,b), bunny, board):
+		x, y = bunny.pos
+		board[x][y] = 0
+		bunny.pos = (a,b)
+		board[a][b] = bunny
+
+	printgrid(board)
+	bunnies = [Bunny() for i in range(5)]
+	
+		obj = bunnysim.Bunny()
+		obj.pos = random.choice(getemptycoords(board))
+		bunnies.append(obj)
+		spawn(obj.pos, obj, board)
+
+	printgrid(board)
+
+
+
+
+if __name__ == '__main__':
+	
+	main()
